188101697220b4c092e8a01ff285b6f1f9e8b930
Rana Moché <fmoche@udesa.edu.ar>
2023-04-03 11:11:11 -0300

    asdf

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
+
+import Footer from './Components/Footer/Footer'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <Footer />
     </div>
   );
 }
