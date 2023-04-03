fca38cefaa68c7b5bd6d37d41383601c11a3ccf9
Rana Moché <fmoche@udesa.edu.ar>
2023-04-03 10:31:10 -0300

    footer base

---
+++ tfi/src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <footer>
+            <p>Moché, Fausto ~ Reid, Kevin Douglas</p>
+        </footer>
+    )
+}
+
+export default Footer
