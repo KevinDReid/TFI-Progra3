58e66c9548dfdaf282982c919d68bbef4016f84a
KevinDReid <kreid@udesa.edu.ar>
2023-04-10 11:10:48 -0300

    ahora hay top rated y popular en home

--- src/Components/Home/MovieList.js
+++ src/Components/Home/MovieList.js
@@ -5,31 +5,51 @@ export default class MovieList extends Component {
         super(props)
         console.log(this.props);
         this.state={
-            datos: props.datos
+            datosTop: props.datosTop,
+            datosPop: props.datosPop
         }
     }
     componentDidUpdate(prevProps) {
-      if (prevProps.datos !== this.props.datos) {
-        this.setState({ datos: this.props.datos });
+      if (prevProps.datosPop !== this.props.datosPop) {
+        this.setState({ datosPop: this.props.datosPop });
+      }
+      if ((prevProps.datosTop !== this.props.datosTop))
+      {
+        this.setState({ datosTop: this.props.datosTop });
+
       }
     }
 
   render() {
     console.log(this.state)
-    if (this.state.datos.length === 0) {
+    if (this.state.datosPop.length === 0 || (this.state.datosTop.length === 0)) {
       return <p>Loading...</p>;
     }
   
     else{
-      console.log(this.state.datos)
+      console.log(this.state.datosPop)
       return (
+      <div className='masterMoviesContainer'>
+          <div className='divCont'>
+          <h2>Top Rated</h2>
+        <ul className='movieSuperContainer'>
+          {
+            this.state.datosTop.map((movie, idx)=><Card key={movie+idx} id={movie.id} name={movie.title} img={'https://image.tmdb.org/t/p/original' + movie.poster_path} desc={movie.overview}/>)
+          }
 
-      <ul className='movieSuperContainer'>
+        </ul>
+        </div>
+        <div className='divCont'>
 
-        {
-            this.state.datos.map((movie, idx)=><Card key={movie+idx} id={movie.id} name={movie.title} img={'https://image.tmdb.org/t/p/original' + movie.poster_path} desc={movie.overview}/>)
-        }
-      </ul>
+          <h2>Popular</h2>
+        <ul className='movieSuperContainer'>
+
+          {
+            this.state.datosPop.map((movie, idx)=><Card key={movie+idx} id={movie.id} name={movie.title} img={'https://image.tmdb.org/t/p/original' + movie.poster_path} desc={movie.overview}/>)
+          }
+        </ul>
+          </div>
+        </div>
     )
   }}
 }
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -7,7 +7,8 @@ export default class Home extends Component {
     constructor(props){
         super(props)
         this.state={
-            datos:[]
+            datosPop:[],
+            datosTop:[]
         }
     }
     componentDidMount(){
@@ -17,7 +18,19 @@ export default class Home extends Component {
                     return response.json()
                 })
                 .then((data)=>{
-                    this.setState({datos: data.results})
+                    this.setState({datosPop: data.results.slice(0,5)})
+                    // console.log(this.state.datos);
+            }
+            )
+                .catch(function(error){
+                console.log('Este es el error: ' + error);})
+            fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=97652419e5f4276e5a5fa6f28df39f8c'
+            )
+                .then(function(response) {
+                    return response.json()
+                })
+                .then((data)=>{
+                    this.setState({datosTop: data.results.slice(0,5)})
                     // console.log(this.state.datos);
             }
             )
@@ -30,7 +43,7 @@ export default class Home extends Component {
         <>
       <Header/>
       <main>
-        <MovieList datos={this.state.datos}/>
+        <MovieList datosPop={this.state.datosPop} datosTop={this.state.datosTop}/>
       </main>
     <Footer/>
         </>
