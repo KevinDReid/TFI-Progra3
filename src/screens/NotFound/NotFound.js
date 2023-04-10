9a9d9ca367326363af52ed09c45f95452ea4f63e
Rana Moché <fmoche@udesa.edu.ar>
2023-04-10 09:14:44 -0300

    footer y not found

--- src/screens/NotFound/NotFound.js
+++ src/screens/NotFound/NotFound.js
@@ -4,7 +4,7 @@ export default class NotFound extends Component {
   render() {
     return (
       <div>
-        
+        <h1 align='center'>404 Contenido Inexistente</h1>
       </div>
     )
   }
