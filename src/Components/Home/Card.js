015a4bb7d19c453c73c534405fdd5a4f98e7e3e0
KevinDReid <kreid@udesa.edu.ar>
2023-04-10 11:34:38 -0300

    cambios en css mas que nada

--- src/Components/Home/Card.js
+++ src/Components/Home/Card.js
@@ -12,17 +12,41 @@ export default class Card extends Component {
             desc: this.props.desc
         }
     }
+
+    changeText(){
+      if(this.state.texto === 'Ver mas'){
+          this.setState({
+              texto: 'Ver menos',
+              clase: 'show'
+          })
+      } else {
+          this.setState({
+              texto: 'Ver mas',
+              clase:'hidden'
+          })
+      }
+  }
+
   render() {
 
     console.log(this.state);
     return (
       <li className='movieContainer' key={this.state.key}>
-        <Link to={'detail/id/:id' + this.state.id}>
+        <Link className='movieLink' to={'detail/id/:id' + this.state.id}>
             <img className='movieImg' src={this.props.img} alt='asd'/>
+          <div>
+
             <div className='movieText'>
             <h4 className='movieTitle'>{this.state.name}</h4>
             <p className='movieDesc'>{this.state.desc}</p>
             </div>
+            <div className='movieButtons'>
+
+            <button>Agregar a favoritos</button>
+            <button>Detalle</button>
+            <button>Ver más</button>
+            </div>
+          </div>
         </Link>
       </li>
     )
