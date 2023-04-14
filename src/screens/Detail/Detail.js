a4d2005f5a4d80c8cc35ba6d7556ede301bc233e
Rana Moché <fmoche@udesa.edu.ar>
2023-04-14 19:36:34 -0300

    detail favorites

--- src/screens/Detail/Detail.js
+++ src/screens/Detail/Detail.js
@@ -1,39 +1,50 @@
-import React, { Component } from 'react'
-import Header from '../../Components/Header/Header'
-import Movie from '../../Components/Detail/Movie'
-import Footer from '../../Components/Footer/Footer'
-import Movie from '../../Components/Detail/Movie'
+import React, { Component } from "react";
+import { ClipLoader } from "react-spinners";
+import Header from "../../Components/Header/Header";
+import Movie from "../../Components/Detail/Movie";
+import Footer from "../../Components/Footer/Footer";
 export default class Detail extends Component {
-  constructor(props){
-      super(props)
-      this.state={
-          id:this.props.match.params.id,
-          movie:''
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: Number(this.props.match.params.id),
+      movie: "",
+    };
   }
 
-  componentDidMount(){
-      fetch(`https://api.themoviedb.org/3/movie/${this.state.id}?api_key=97652419e5f4276e5a5fa6f28df39f8c`)
-
-      .then(resp => resp.json())
-      .then(data => this.setState({
-          movie:data
-      }))
+  componentDidMount() {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${this.state.id}?api_key=97652419e5f4276e5a5fa6f28df39f8c`
+    )
+      .then((resp) => resp.json())
+      .then((data) =>
+        this.setState({
+          movie: data,
+        })
+      );
   }
 
   render() {
-    console.log(this.state.movie)
+    console.log(this.state.movie);
     return (
-        <>
-        <Header/>
-      <main>
-          
-          <Movie use={this.state.movie}/>
-          
-      </main>
-      
-      <Footer/>
+      <>
+        <Header />
+        <main>
+          {this.state.isLoading ? (
+            <div className="loader-container">
+              <ClipLoader
+                color="#00BFFF"
+                loading={this.state.isLoading}
+                size={150}
+              />
+            </div>
+          ) : (
+            <Movie use={this.state.movie} id={this.state.id} />
+          )}
+        </main>
+
+        <Footer />
       </>
-    )
+    );
   }
 }
--- src/screens/Favorites/Favorites.js
+++ src/screens/Favorites/Favorites.js
@@ -1,54 +1,76 @@
-import React, { Component } from 'react'
-import Header from '../../Components/Header/Header'
-import Footer from '../../Components/Footer/Footer'
-import Card from '../../Components/Home/Card'
-
+import React, { Component } from "react";
+import { ClipLoader } from "react-spinners";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+import Card from "../../Components/Home/Card";
+import "./style.css";
 
 class Favorites extends Component {
-    constructor(props){
-        super(props)
-        this.state= {
-            favorites: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: [],
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    let storage = localStorage.getItem("favorites");
+    if (storage !== null) {
+      let storageToArray = JSON.parse(storage);
+      Promise.all(
+        storageToArray.map((id) => {
+          return fetch(
+            `https://api.themoviedb.org/3/movie/${id}?api_key=97652419e5f4276e5a5fa6f28df39f8c`
+          )
+            .then((resp) => resp.json())
+            .then((data) => data);
+        })
+      )
+        .then((data) => {
+          console.log(data);
+          this.setState({
+            data: data,
+          });
+        })
+        .catch((err) => console.log(err));
     }
+  }
 
-    componentDidMount(){
-        let storage = localStorage.getItem('favorites')
-        if(storage !== null){
-            let storageToArray = JSON.parse(storage)
-            Promise.all(storageToArray.map(id => {
-                    return(
-                      fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=97652419e5f4276e5a5fa6f28df39f8c`)
-                        .then(resp => resp.json())
-                        .then(data => data)
-                    )
-                })
-            )
-            .then(data => this.setState({
-                favoritos: data
-            }))
-            .catch(err => console.log(err))
-
-
-        }
-    }
   render() {
     return (
       <>
-      <Header/>
-      <Card
-            key= {this.props.key}
-            id={this.props.id}
-            name= {this.props.name}
-            img= {this.props.img}
-            desc= {this.props.desc}
-            text= {"Ver mas"}
-            class= {"hidden"}
-      />
-      <Footer/>
+        <Header />
+        {this.state.isLoading ? (
+          <div className="loader-container">
+            <ClipLoader
+              color="#00BFFF"
+              loading={this.state.isLoading}
+              size={150}
+            />
+          </div>
+        ) : this.state.data.length > 0 ? (
+          this.state.data.map((el, idx) => {
+            console.log(el);
+            return (
+              <Card
+                key={el + idx}
+                id={el.id}
+                name={el.title || el.name}
+                img={el.poster_path}
+                desc={el.overview}
+                text={"More"}
+                class={"hidden"}
+              />
+            );
+          })
+        ) : (
+          <h2 className="empty">Favorites is empty</h2>
+        )}
+        <Footer />
       </>
-    )
+    );
   }
 }
 
-export default Favorites
+export default Favorites;
